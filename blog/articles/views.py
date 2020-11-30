00bab3322b9083e1b218e30543e4f8c1d27f7ea9
turistik75rus <turistik75rus@mail.ru>
2020-11-30 18:16:53 +0300

    Laba3

---
+++ blog/articles/views.py
@@ -0,0 +1,6 @@
+from articles import models
+from articles.models import Article
+from django.shortcuts import render
+def archive(request):
+     return render(request, 'archive.html', {"posts":Article.objects.all()})
+# Create your views here.
